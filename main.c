f72e59421111ef291c05ed9f4b6e4d99d070680a
hyerin2411931 <2411931@sookmyung.ac.kr>
2025-11-06 16:00:26 +0900

    Week10_06

--- main.c
+++ main.c
@@ -6,16 +6,24 @@ int main(int argc, char *argv[])
 {
   char input[100];
   FILE*fp;
-  fp = fopen("sample.txt", "w");
-  int i;
+  char c;
   
-  for(i =0; i <3; i++){
-  printf("input a word:");
-  scanf("%s", input);   //배열에는 & 안씀
-  fprintf(fp, "%s\n", input); }
-  
-  fclose(fp); 
-   
+  fp = fopen("sample.txt", "r");
+  if(fp == NULL){
+    printf("failed to open file.\n");
+    return  -1;}
+
+#if 0   
+  while((c = fgetc(fp)) != EOF){    //한글자씩 가져오는 경우 
+   putchar(c);
+   }
+#endif
+
+#if 1
+  while( fgets(input,100, fp) > 0)  //한 단어씩 가져오는 경우, 글자수가 0이 아닐때, or 0 이상일떄 
+  printf("%s",input); 
+
+#endif
   system("PAUSE");	
   return 0;
 }
