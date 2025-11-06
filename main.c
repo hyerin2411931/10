05907799dcb3abc0ed044c5651af2dba6805c874
hyerin2411931 <2411931@sookmyung.ac.kr>
2025-11-06 15:31:23 +0900

    Week10_04

--- main.c
+++ main.c
@@ -4,12 +4,9 @@
  
 int main(int argc, char *argv[])
 {
-  char stc[] = "The worst things to eat before you sleep";
-  char dst[100];
+  char str[30] = "happy C programming";
   
-  strcpy(dst, stc);   //앞에 반환값 받는 변수 설정 안해도 됨. 
-   
-  printf("copied string : %s\n", dst);
+  printf("문자열\"%s\"의 길이: %i\n", str, strlen(str)); 
    
   system("PAUSE");	
   return 0;
